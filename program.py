18cad2e81f7803930ad167c583e042fe8e605480
sandeep sharma <sandeepsha@hcl.com>
2022-01-31 11:51:08 +0530

    Added log and Readme files

--- program.py
+++ program.py
@@ -7,7 +7,10 @@ data = json.loads(myfile.read())
 
 class Validate:
      
-    def __init__(self,*ip_addr):
+    def __init__(self):
+        pass
+        
+    def validate_ip(self,*ip_addr):
         self.ip_addr = ip_addr
         
         print("Recived Ip from json file:",ip_addr[0])
@@ -15,13 +18,19 @@ class Validate:
         
         for i in range(len(ip_addr[0])):
             print("Validating IP",ip_addr[0][i])
+            
             try:
                 if(re.search(regex,ip_addr[0][i])):
-                    print("ip is valid,Validating the ping")
+                    if (re.search(r"10|172|192",ip_addr[0][i].split('.')[0])):
+                        print("This is Reserved IP")
+                            
+                    print("ip is valid, proceeding for the ping")
                     os.system("ping -n 1 "+ip_addr[0][i])
                 else:
                     raise Exception()
             except:
                 print("ip is not valid")
+            print(end="\n")
     
-Validate(data['ipaddr'])
+ipObj=Validate()
+ipObj.validate_ip(data['ipaddr'])
